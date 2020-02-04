ba0eec2ac7ea7c0ae5bfd564ba86a0f7b53768ba
Jonas Mortensen <Jonasmortensen3@gmail.com>
2020-02-04 15:52:44 +0100

    added tamaki command

--- homomorphism/src/tamaki-2017.cpp
+++ homomorphism/src/tamaki-2017.cpp
@@ -10,6 +10,12 @@
 
 std::string Tamaki2017::decompose(std::shared_ptr<Graph> g)
 {
-    system("touch HERE.txt");
+    //This might not be the smartest, but the easiest to get to run
+    
+    //Write graph to tmp file
+    //Feed to Tamaki-2017
+    //Read output and remove tmp file
+    
+    system("java -Xmx30g -Xms30g -Xss10m tw.exact.MainDecomposer < ex001.gr > ex001.td");
     return "This is a td";
 }
