9327ae5418683a9e7dc31028812a46b885080856
Bacontyven <chle@itu.dk>
2020-02-05 13:50:31 +0100

    Spasm created based on partitions

--- homomorphism/include/homomorphism/spasm.h
+++ homomorphism/include/homomorphism/spasm.h
@@ -9,18 +9,21 @@
 class Spasm
 {
 public:
-    Spasm(std::vector<std::pair<Graph, size_t> > g) : graphs_(g) {};
+    Spasm(std::vector<std::pair<std::shared_ptr<Graph>, int> > g) : graphs_(g) {};
 
     static std::shared_ptr<Spasm> fromFile(std::string path);
-    static std::shared_ptr<Spasm> createSpasm(Graph H);
+    static std::shared_ptr<Spasm> createSpasm(std::shared_ptr<Graph> H);
     static std::shared_ptr<Spasm> testSpasm();
 
     std::size_t size();
-    std::pair<Graph, size_t>& operator[](std::size_t position);
+    std::pair< std::shared_ptr<Graph>, int>& operator[](std::size_t position);
     std::string toString();
+    void prettyPrint(std::ostream& os);
 
 private:
-    std::vector<std::pair<Graph, size_t> > graphs_;
+    std::vector<std::pair<std::shared_ptr<Graph>, int> > graphs_;
+
+    static void addPartitioningsRec(std::shared_ptr<Graph> H, std::vector<std::pair<std::shared_ptr<Graph>, int>>& graphs, std::set<size_t>* parts, size_t next, size_t size);
 };
 
 #endif
