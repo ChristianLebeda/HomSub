c2fe779105e060d2a3d525acdcfa7c26433d7c43
Jonas Mortensen <Jonasmortensen3@gmail.com>
2020-02-25 12:49:14 +0100

    Added piping solution to tamaki

--- homomorphism/src/tamaki-2017.cpp
+++ homomorphism/src/tamaki-2017.cpp
@@ -1,30 +1,99 @@
 #include "homomorphism/tamaki-2017.h"
-
-#include <fstream>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<signal.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+#include <sstream>
 #include <iostream>
 
 std::shared_ptr<TreeDecomposition> Tamaki2017::decompose(std::shared_ptr<Graph> g)
 {
-    //This might not be the smartest way, but it is the easiest way
+    //Create pipes for read and write
+    int inp[2], outp[2];
+    pipe(inp);
+    pipe(outp);
+
+    //Fork
+    switch (fork()) {
+    case -1:
+        perror("fork");
+        exit(EXIT_FAILURE);
+
+    case 0:
+        //child Process
+        close( inp[0]); close(1); dup( inp[1]); close( inp[1]);
+        close(outp[1]); close(0); dup(outp[0]); close(outp[0]);
+
+        execlp("java", "-cp /Users/jonasmortensen/Documents/Thesis/SubgraphThesis/build/test/Debug -Xmx30g -Xms30g -Xss10m", "tw.exact.MainDecomposer", (const char*)NULL);
+
+        perror("exec");
+        exit(EXIT_FAILURE);
+    }
+        
+    // parent process
     
+    // write to outp[1]; read from inp[0]
+    FILE* fin  = fdopen(inp[0], "r");
+    FILE* fout = fdopen(outp[1], "w");
     
-    std::ofstream graphFile;
+    fprintf(fout, "p tw 4 4\n1 2\n2 3\n3 4\n4 1\n");
     
-    graphFile.open ("tempGraph.gr");
-    graphFile << g->toGr();
-    graphFile.close();
-    system("java -Xmx30g -Xms30g -Xss10m tw.exact.MainDecomposer < tempGraph.gr > tempTree.td");
-    remove("tempGraph.gr");
+    fclose(fout);
     
-    std::string line;
-    std::ifstream tdFile ("tempTree.td");
-    std::shared_ptr<TreeDecomposition> td;
-    if (tdFile.is_open())
+    char buffer [256];
+    if (fin == NULL) perror ("Error opening file");
+    else
     {
-        td = TreeDecomposition::parseTd(tdFile);
-        tdFile.close();
-    } else std::cout << "Unable to open file";
-    
-    remove("tempTree.td");
-    return td;
+        size_t bagN, width, n;
+        std::shared_ptr <EdgeSetGraph> G;
+        
+        std::vector<std::unordered_set<size_t>> bags;
+        
+        int i = -1;
+        size_t u, v;
+        while ( ! feof (fin) )
+        {
+            if ( fgets (buffer , 256 , fin) == NULL ) break;
+            std::string line = buffer;
+            if(i < 0) { //Read header
+                if (!std::sscanf(line.c_str(), "s td %zd %zd %zd", &bagN, &width, &n)) return nullptr;
+                i = 0;
+                bags.resize(bagN);
+                G = std::make_shared<EdgeSetGraph>(bagN);
+            } else if (i < bagN) { //Read bag
+                int bagI = -1;
+                std::stringstream ss;
+                ss << line;
+                std::string temp;
+                size_t util;
+                while(!ss.eof())
+                {
+                    ss >> temp;
+                    
+                    if(std::stringstream(temp) >> util)
+                    {
+                        if(bagI < 0) bagI = util - 1;
+                        else bags[bagI].insert(util);
+                    }
+                    
+                    temp="";
+                }
+                bagI = -1;
+                i++;
+            } else { //Read edge
+                if (!std::sscanf(line.c_str(), "%zd %zd", &u, &v)) {
+                    std::cout << line << std::endl;
+                    return nullptr;
+                }
+
+                G->addEdge(u - 1, v - 1);
+            }
+            
+        }
+        fclose (fin);
+        return std::make_shared<TreeDecomposition>(G, bags, width);
+    }
+    return nullptr;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -3,18 +3,24 @@
 #include "homomorphism/adjacency_matrix_graph.h"
 #include "test/graph_generator.h"
 #include <time.h>
+#include "homomorphism/tree_decomposition.h"
+#include "homomorphism/tamaki-2017.h"
 
 int main(int argc, char *argv[])
 {
-    srand(time(NULL));
+    //srand(time(NULL));
     
-    std::shared_ptr<AdjacencyMatrixGraph> g = std::make_shared<AdjacencyMatrixGraph>(1);
+    //std::shared_ptr<AdjacencyMatrixGraph> g = std::make_shared<AdjacencyMatrixGraph>(1);
 
+    std::shared_ptr<Graph> g = AdjacencyMatrixGraph::testGraph();
+    Tamaki2017 t;
+    std::shared_ptr<TreeDecomposition> td = t.decompose(g);
     
-    GraphGenerator gg;
-    gg.RandomConnectedGraph(g, 10, 20);
+    td->getGraph()->prettyPrint(std::cout);
+    //GraphGenerator gg;
+    //gg.RandomConnectedGraph(g, 10, 20);
     
-    g->prettyPrint(std::cout);
+    //g->prettyPrint(std::cout);
     //std::shared_ptr<Graph> g = AdjacencyMatrixGraph::testGraph();
     
     return 0;
