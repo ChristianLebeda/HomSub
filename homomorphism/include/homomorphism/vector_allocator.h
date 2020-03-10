b3eac611aa9e5cd52ac47e5b597a046a336034ea
chle <chle@itu.dk>
2020-03-10 16:23:42 +0100

    Allocator added. Can be extended with caching

---
+++ homomorphism/include/homomorphism/vector_allocator.h
@@ -0,0 +1,16 @@
+#ifndef HOMOMORPHISM_VECTOR_ALLOCATOR_H
+#define HOMOMORPHISM_VECTOR_ALLOCATOR_H
+
+#include <vector>
+
+// Can be extended for caching
+class VectorAllocator {
+public:
+    void setSize(size_t n, size_t maxb);
+    std::vector<size_t> get(size_t b);
+    void free(std::vector<size_t>& vector, size_t b);
+private:
+    std::vector<size_t> sizes_;
+};
+
+#endif
