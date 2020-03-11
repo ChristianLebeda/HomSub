e255c34742c3fe98323c321490acd342411b5e09
Jonas Mortensen <Jonasmortensen3@gmail.com>
2020-03-11 14:03:54 +0100

    added log mode csv

--- experiments/include/experiments/csv_logger.h
+++ experiments/include/experiments/csv_logger.h
@@ -1,5 +1,23 @@
 #ifndef CSV_LOGGER_H
 #define CSV_LOGGER_H
 
+#include "experiments/test-logger.h"
+
+
+class CSVLogger : public TestLogger {
+public:
+    CSVLogger(std::ostream& stream) : stream_(stream), currentTestName_("") {};
+    void NotifyTestStart(std::string name);
+    void NotifyTestEnd(float duration);
+    void NotifyTestSubstep(SubStep step, std::string note, float duration);
+    void NotifyTestIterative(int n, std::string note, float duration);
+    void NotifyTestAssert(std::string note,bool passed);
+    void NotifyFailed();
+    void NotifyRunStart();
+private:
+    std::ostream &stream_;
+    std::string currentTestName_;
+    void logCSV(std::string info, std::string comment, std::string result);
+};
 
 #endif /* csv_logger_h */
--- experiments/src/csv_logger.cpp
+++ experiments/src/csv_logger.cpp
@@ -6,3 +6,47 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+#include "experiments/csv_logger.h"
+
+void CSVLogger::NotifyTestStart(std::string name)
+{
+    currentTestName_ = name;
+}
+
+void CSVLogger::NotifyTestEnd(float duration)
+{
+    
+}
+
+void CSVLogger::NotifyTestSubstep(SubStep step, std::string note, float duration)
+{
+    logCSV(TestLogger::subStepToString(step), note, std::to_string(duration));
+}
+
+void CSVLogger::NotifyTestIterative(int n, std::string note, float duration)
+{
+    logCSV("n = " + std::to_string(n), note, std::to_string(duration));
+}
+
+void CSVLogger::NotifyTestAssert(std::string note, bool passed)
+{
+    if(passed) {
+        logCSV("", note, "PASSED");
+    } else {
+        logCSV("", note, "FAILED");
+    }
+}
+
+void CSVLogger::NotifyFailed()
+{
+}
+
+void CSVLogger::NotifyRunStart()
+{
+    stream_ << "testName, info, coment, result" << std::endl;
+}
+
+void CSVLogger::logCSV(std::string info, std::string comment, std::string result) {
+    stream_ << currentTestName_ << ", " << info << ", " << comment << ", " << result << std::endl;
+}
