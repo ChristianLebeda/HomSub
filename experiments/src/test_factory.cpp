567072c1bb5af0c44b9f9f5ce4d1f8978aaec1fa
Jonas Mortensen <Jonasmortensen3@gmail.com>
2020-05-07 12:30:23 +0200

    Removed old print

--- experiments/src/test_factory.cpp
+++ experiments/src/test_factory.cpp
@@ -133,8 +133,7 @@ void TestFactory::SquaresInGrid(TestSettings& settings, TestLogger& logger)
     for(int n = 1; n < 17; n = n * 2) {
         GraphGenerator::CompleteGrid(g, n, n);
         REPEATED_CLOCK_START;
-        //Main::subgraphsGraph(h, g);
-        std::cout << Main::subgraphsGraphMaxDegree(h, g) << std::endl;
+        Main::subgraphsGraph(h, g);
         REPEATED_CLOCK_END;
         for(int d : durations) {
             logger.Log("", n, 4, d);
