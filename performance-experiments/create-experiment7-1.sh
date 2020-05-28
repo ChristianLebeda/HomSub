318505083543b3ffd8aeb1be7e2e8ea4ecd72d16
Jonas Mortensen <Jonasmortensen3@gmail.com>
2020-05-28 14:39:52 +0200

    finished exp 7

--- performance-experiments/create-experiment7-1.sh
+++ performance-experiments/create-experiment7-1.sh
@@ -5,8 +5,8 @@ cd experiment7-1
 cp ../utilities/runhomomorphisms.sh run.sh
 cp ../patterns/cycle7.gr pattern/
 ./prepare.sh cycle7
-python3 ../trimspasm.py pattern/cycle7.spsmd > tmp
+python3 ../utilities/trimspasm.py pattern/cycle7.spsmd > tmp
 mv tmp pattern/cycle7.spsmd
-python3 rungen.py 2 15 1 0.001 1000 10
+python3 rungen.py 2 15 1 0.001 1000 1
 rm input/*/*.grf
 rm input/*/*.lad
